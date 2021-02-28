83787eaf1fd4b74175c82323b6ec7f3c9917a3bd
shushikata <shu.42111227@gmail.com>
2021-03-01 02:23:37 +0900

    ideas-controller-indexの実装

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -5,6 +5,18 @@ module Api
     # ideas_class
     class IdeasController < ApplicationController
       def index
+        if params[:category_name].present?
+          category = Category.find_by(name: params[:category_name])
+          if category.present?
+            @ideas = category.ideas
+            render json: @ideas, each_serializer: IdeaSerializer
+          else
+            head :not_found
+          end
+        else
+          @ideas = Idea.eager_load(:category)
+          render json: @ideas, each_serializer: IdeaSerializer
+        end
       end
 
       def create
---
+++ app/serializers/category_serializer.rb
@@ -0,0 +1,3 @@
+class CategorySerializer < ActiveModel::Serializer
+  attribute :name
+end
